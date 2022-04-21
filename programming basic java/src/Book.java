d118bd01611b603dd0ed230608e86798d8a79245
LeeMin-hyeong <lmhstrar070@naver.com>
2022-04-21 12:15:32 +0900

    java

---
+++ programming basic java/src/Book.java
@@ -0,0 +1,19 @@
+public class Book{
+    String title;
+    String author;
+
+    public Book(String t){
+        titel=t;
+        author="작자미상";
+    }
+
+    public Book(String t, String  a){
+        title=t;
+        author=a;
+    }
+
+    public static void main(String[] args) {
+        Book littlePrince=new Book("어린왕자")
+    }
+}
+
---
+++ programming basic java/src/Circle.java
@@ -0,0 +1,16 @@
+public class Circle {
+    int radius;
+    void set(int r){radius=r;}
+    double getArea(){return 3.14*radius*radius;}
+
+    public Circle(){radius=0;}
+    public Circle(int r){radius=r;}
+
+    public static void main(String[] args) {
+        Circle pizza=new Circle(10);
+        System.out.println(pizza.getArea());
+
+        Circle donut=new Circle();
+        System.out.println(donut.getArea());
+    }
+}
